53c5b433d431bbd890fac4310143a84de2ff79be
Sollykhan <poletti.nikolas@gmail.com>
2026-04-15 08:33:32 +0200

    Correction du style PEP 8 (lignes vides)

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,18 @@
 from calculator.calculator import add, divide, multiply, subtract
 
+
 def test_addition():
     assert add(2, 3) == 5
+
 
 def test_subtraction():
     assert subtract(10, 5) == 5
 
+
 def test_multiplication():
     assert multiply(4, 6) == 24
 
+
 def test_division():
     assert divide(10, 2) == 5
+    
